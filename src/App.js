4b2feeec56cff76f2feb12f77ba029f35bbd404d
Ashley Radford <aradford7@outlook.com>
2019-05-03 14:33:24 -0700

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
+//1.bring in React and Component instance from react above!
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
     </div>
   );
 }
 
-export default App;
+//2.define our Class (Post in this case) Component
+class  Post extends Component{
+  //3.what should the component render
+  render () {
+    //Make sure to return some UI
+    return(
+      <h1>This is Post component</h1>
+    )
+  }
+}
+//5. Now you have created template for a component, go to src/index.jsto load and 
+//render the new component
+//6. change 3rd import to import Hello from './App.
+//4.export default App;
+export default Post
